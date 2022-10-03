c38b5beb2194fe04b6763822dabed5b7e7fcffd9
divya <f20212999@goa.bits-pilani.ac.in>
2022-10-03 08:37:02 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./styles.css";
+import React from "react";
+
+var shoppingList = [
+  "milk",
+  "egg",
+  "makhana",
+  "nutrichoice biscuits",
+  "dettol",
+  "toothbrush",
+  "microbio notes",
+];
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>print my shopping list</h1>
+      <ol>
+        {shoppingList.map(function (item) {
+          console.log(item);
+          return <li>{item}</li>;
+        })}
+      </ol>
     </div>
   );
 }
